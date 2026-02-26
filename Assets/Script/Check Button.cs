c6c9f59bef4898354f2bed67c440c590178be37c
Mikubu39 <nicek92408@gmail.com>
2026-02-26 15:09:03 +0700

    demo part 5

---
+++ Assets/Script/Check Button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CheckButton : MonoBehaviour
+{
+    public void button()
+    {
+        Debug.Log("lmao");
+    }
+}
